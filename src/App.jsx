009a4675e7829ad35aed5e1fdefd00d2f357322f
ProfaIngrid <134079588+ProfaIngrid@users.noreply.github.com>
2024-03-05 08:22:35 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Logo from './images/logo.png';
+import Hermione from './images/hermione.jpg';
+import Harry from './images/harry.jpg';
+import Ronald from './images/ronald.jpg';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+const Main = () => {
+    return(
+        <main>
+            <div>
+                <img src={Hermione}/>
+                <h2>Hermione</h2>
+            </div>
+            <div>
+                <img src={Harry}/>
+                <h2>Harry</h2>
+            </div>
+            <div>
+                <img src={Ronald}/>
+                <h2>Ronald</h2>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
